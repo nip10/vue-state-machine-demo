e8ca4d34fcf14865815309412d52a1da4b60ee12
nip10 <diogocardoso92@outlook.com>
2025-02-16 23:54:54 +0000

    lint

--- src/components/00-bad/BadCart.vue
+++ src/components/00-bad/BadCart.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import type { CartItem } from "@/types";
+
+const api = {
+  addItem: (_: any) => Promise.resolve(),
+};
+
+// ACTUAL CODE BELOW
 const isLoading = ref(false);
 const isError = ref(false);
 const items = ref<CartItem[]>([]);
 const isCheckingOut = ref(false);
 
-const addToCart = async (product: Product) => {
+const addToCart = async (product: CartItem) => {
   isLoading.value = true;
   isError.value = false;
   try {
@@ -20,4 +28,7 @@ const addToCart = async (product: Product) => {
 // Show invalid states possible:
 console.log(isLoading.value && isCheckingOut.value); // Can be true!
 console.log(isError.value && isCheckingOut.value); // Can be true!
+
+// IGNORE - DEMO
+addToCart({ id: 1, name: "Product 1" } as any);
 </script>
